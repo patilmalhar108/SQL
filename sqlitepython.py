157b79ee35aa7c86f68cd3b07296d39affe4ad3b
patilmalhar108 <patilmalhar.a@gmail.com>
2026-05-31 12:50:24 -0500

    Add files via upload

---
+++ sqlitepython.py
@@ -0,0 +1,7 @@
+print("What is the type of above given database?\n 1: Relational database\n 2: Non relational database")
+answer = int(input("Enter your guess: "))
+if answer == 2:
+    print("Correct!")
+else:
+    print("Incorrect")
+print("Tell your mentor why you guessed this")
